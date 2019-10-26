7634514dca36e3c20d4db11cf6c10641e00f5c85
qjm <695590221@qq.com>
2019-10-26 15:26:54 +0800

    zengjia2

--- springsecurity_demo/src/test/java/com/itheima/test/test02.java
+++ springsecurity_demo/src/test/java/com/itheima/test/test02.java
@@ -5,6 +5,6 @@ public class test02 {
     public static void main(String[] args) {
         git_test git_test = new git_test();
         System.out.println(git_test);
-        System.out.println(1111);
+        System.out.println(2222);
     }
 }
