78e5dd66fb806ec1c5b39451f4648d0399a6f175
qjm <695590221@qq.com>
2019-10-26 15:22:20 +0800

    zengjia2

--- springsecurity_demo/src/test/java/com/itheima/test/test03.java
+++ springsecurity_demo/src/test/java/com/itheima/test/test03.java
@@ -1,7 +1,7 @@
 package com.itheima.test;
 
 
-public class test02 {
+public class test03 {
     public static void main(String[] args) {
         git_test git_test = new git_test();
         System.out.println(git_test);
