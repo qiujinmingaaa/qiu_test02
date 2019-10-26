f3b2d880a3ea1078ffa2809095d133db8cea6774
qjm <695590221@qq.com>
2019-10-26 15:17:32 +0800

    改变

--- springsecurity_demo/src/test/java/com/itheima/test/git_test.java
+++ springsecurity_demo/src/test/java/com/itheima/test/git_test.java
@@ -1,17 +1,12 @@
 package com.itheima.test;
 
-/**
- * @program: health_parent
- * @description:
- * @author:
- * @create: 2019-10-26 14:18
- **/
+
 public class git_test {
     public static void main(String[] args) {
         System.out.println("git");
     }
     public void f(){
-        System.out.println("增加");
+        System.out.println("增加测试");
     }
 
 
