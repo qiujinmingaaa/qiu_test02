fed8c392b2fee8aebcb242ac2033f0bc1cc04ebf
qjm <695590221@qq.com>
2019-10-26 14:57:21 +0800

    增加了测试

---
+++ springsecurity_demo/src/test/java/com/itheima/test/git_test.java
@@ -0,0 +1,16 @@
+package com.itheima.test;
+
+/**
+ * @program: health_parent
+ * @description:
+ * @author:
+ * @create: 2019-10-26 14:18
+ **/
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+    public void f(){
+        System.out.println("增加内容");
+    }
+}
